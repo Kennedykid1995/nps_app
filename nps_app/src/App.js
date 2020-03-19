23ad4007078eb473168b2229f2321acd9a8ba27f
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-19 15:27:54 -0500

    working on the card components

--- nps_app/src/App.js
+++ nps_app/src/App.js
@@ -2,8 +2,31 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios'; 
 import {Link} from 'react-router-dom'; 
+import styled from 'styled-components'
 
 const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=26cf207a661b4974a24e4a4ae5259757'
+
+const NewsCard = styled.div`
+  width: 300px; 
+  height: 150px; 
+  border: 1px solid black;
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center; 
+  text-align: left; 
+`
+const NewsImg = styled.img`
+  width: 125px; 
+  height: 125px
+  border: 1 px solid red;
+  padding: 5px; 
+`
+const NewsTitle = styled(Link)`
+  fontSize: 20px;
+  fontWeight: bold;
+  color: black; 
+  text-decoration: none; 
+`
 
 const App = () => {
   const [data, setData] = useState([]); 
@@ -25,10 +48,12 @@ const App = () => {
       //image
       //title
       //URL link within the title.
-      <div key={article.publishedAt}>
-        <img src={article.urlToImage} />
-        <Link to={article.url}>{article.title}</Link>
-      </div>
+      <NewsCard key={article.publishedAt}>
+        <NewsImg src={article.urlToImage} />
+        <section>
+        <NewsTitle to={article.url}>{article.title}</NewsTitle>
+        </section>
+      </NewsCard>
     ))}
     </div> 
   );
