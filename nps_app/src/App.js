fa8d46fc3f3a89b9829cda7af1a1f91cf19df4e3
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-24 14:52:32 -0500

    working on the components

--- nps_app/src/App.js
+++ nps_app/src/App.js
@@ -2,13 +2,14 @@ import React, {useState, useEffect} from 'react';
 import MainHeadline from './components/mainHeadline'; 
 import FourHeadlines from './components/fourHeadlines'; 
 import BigHeadline from './components/bigHeadline'; 
+import ScrollSection from './components/scrollSection'
 const App = () => {
   return (
     <div className="App">
-    NEWS APP
     <MainHeadline />
     <FourHeadlines />
     <BigHeadline />
+    <ScrollSection />
     </div> 
   );
 }
---
+++ nps_app/src/components/scrollSection.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=26cf207a661b4974a24e4a4ae5259757'
+
+const NewsCard = styled.div`
+  width: auto; 
+  height: auto; 
+  border: 1px solid pink;
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center; 
+  text-align: left; 
+  margin: 10px; 
+`
+const NewsTitle = styled(Link)`
+  fontSize: 20px;
+  fontWeight: bold;
+  color: black; 
+  text-decoration: none; 
+`
+
+const ScrollSection = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(url)
+            .then(res => {
+                setData(res.data.articles);
+                console.log(res.data.articles)
+            });
+    }, [])
+    return (
+        <div>
+            {data.slice(6).map(article => (
+                //each of the articles should display:
+                //image
+                //title
+                //URL link within the title.
+                <NewsCard key={article.publishedAt}>
+                    <section>
+                        <NewsTitle to={article.url}>{article.title}</NewsTitle>
+                    </section>
+                </NewsCard>
+            ))}
+        </div>
+    )
+
+}
+
+export default ScrollSection; 
